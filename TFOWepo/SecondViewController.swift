65c073ba87d7b75e70cbd7b66405982acd4bf9c6
dgleiche <dgleiche@me.com>
2016-08-15 00:51:59 -0400

    Initial Commit

---
+++ TFOWepo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TFOWepo
+//
+//  Created by Dylan on 8/15/16.
+//  Copyright © 2016 DIG Productions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
